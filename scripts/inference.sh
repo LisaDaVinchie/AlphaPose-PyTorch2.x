9e18ce278a7e65f55c995068be36ba85a4110106
LisaDaVinchie <lisadavinchie@gmail.com>
2026-04-30 12:16:09 +0200

    Updated

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,13 +1,18 @@
 set -x
 
-CONFIG=$1
-CKPT=$2
-VIDEO=$3
-OUTDIR=${4:-"./examples/res"}
+CONFIG='./configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml' # 61.92 it/s 44.64
+CKPT='./model_files/fast_res50_256x192.pth'
+# CONFIG='./configs/halpe_coco_wholebody_136/resnet/256x192_res50_lr1e-3_2x-regression.yaml' # 55.67it/s
+# CKPT='./model_files/multi_domain_fast50_regression_256x192.pth'
+VIDEO='./data/video3.avi'
+OUTDIR='./'
 
-python scripts/demo_inference.py \
+python -m scripts.demo_inference \
     --cfg ${CONFIG} \
     --checkpoint ${CKPT} \
     --video ${VIDEO} \
     --outdir ${OUTDIR} \
-    --detector yolo  --save_img --save_video
+    --detector yolo \
+    --save_video --sp --vis_fast \
+    --detbatch 1 \
+    --posebatch 8 \
