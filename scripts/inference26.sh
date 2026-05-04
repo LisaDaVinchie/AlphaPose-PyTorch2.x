4e121ecc5e2afadceae154540be15cfe1258e3fc
LisaDaVinchie <lisadavinchie@gmail.com>
2026-05-04 16:28:21 +0200

    Changed video

--- scripts/inference26.sh
+++ scripts/inference26.sh
@@ -4,7 +4,7 @@ CONFIG='./configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml' # 61.92 it/s 44.64
 CKPT='./model_files/fast_res50_256x192.pth'
 # CONFIG='./configs/halpe_coco_wholebody_136/resnet/256x192_res50_lr1e-3_2x-regression.yaml' # 55.67it/s
 # CKPT='./model_files/multi_domain_fast50_regression_256x192.pth'
-VIDEO='./data/video3.avi'
+VIDEO='./data/video1.mp4'
 OUTDIR='./'
 
 python -m scripts.inference_yolo26 \
