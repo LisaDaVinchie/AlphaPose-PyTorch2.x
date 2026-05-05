60535b411f67c6c30abdd4e6cd5a2bca475955de
LisaDaVinchie <lisadavinchie@gmail.com>
2026-05-05 09:15:54 +0200

    Changed weight

--- scripts/inference26.sh
+++ scripts/inference26.sh
@@ -4,8 +4,8 @@ CONFIG='./configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml' # 61.92 it/s 44.64
 CKPT='./model_files/fast_res50_256x192.pth'
 # CONFIG='./configs/halpe_coco_wholebody_136/resnet/256x192_res50_lr1e-3_2x-regression.yaml' # 55.67it/s
 # CKPT='./model_files/multi_domain_fast50_regression_256x192.pth'
-DET_WEIGHTS='./detector/yolo26/data/yolo26x.pt'
-VIDEO='./data/video3.avi'
+DET_WEIGHTS='./detector/yolo26/data/best26.pt'
+VIDEO='./data/video1.mp4'
 OUTDIR='./'
 
 python -m scripts.inference_yolo26 \
