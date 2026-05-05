63c2261f40dad29fcdda6e588cc3b697fbb85f7d
LisaDaVinchie <lisadavinchie@gmail.com>
2026-05-05 12:35:49 +0200

    Deleted comments

--- scripts/inference26.sh
+++ scripts/inference26.sh
@@ -2,8 +2,6 @@ set -x
 
 CONFIG='./configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml' # 61.92 it/s 44.64
 CKPT='./model_files/fast_res50_256x192.pth'
-# CONFIG='./configs/halpe_coco_wholebody_136/resnet/256x192_res50_lr1e-3_2x-regression.yaml' # 55.67it/s
-# CKPT='./model_files/multi_domain_fast50_regression_256x192.pth'
 DET_WEIGHTS='./detector/yolo26/data/best26.pt'
 VIDEO='./data/video1.mp4'
 OUTDIR='./'
